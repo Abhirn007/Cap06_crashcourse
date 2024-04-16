5bc2fab1f0d6a6b88fd00674aa8e69fc08f966e6
myusername <148369766+Abhirn007@users.noreply.github.com>
2024-04-16 22:20:24 +0530

    Async

--- 4- nteractive Websites/2- Ring Bell/app.js
+++
@@ -1,7 +0,0 @@
-<script>
-  document.getElementById("scamImage").addEventListener("click", function() {
-    if (confirm("The website you are going to visit may harm your computer, do you still want to redirect?")) {
-      window.location.href = "https://www.example.com"; // Replace "https://www.example.com" with the actual URL you want to redirect to
-    }
-  });
-</script>
